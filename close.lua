8d6e2eb862db5adfee54760f9469539b2282e6e1
cattium <151743564+cattium@users.noreply.github.com>
2024-03-16 21:48:03 +0400

    Update close.lua

--- close.lua
+++ close.lua
@@ -1,3 +1,32 @@
-socket_http = require "http.compat.socket"
-body = assert(socket_http.request("https://api.ipify.org"))
-print(body)
+
+if game.CoreGui:FindFirstChild("CatHubOpenClose") then
+    game.CoreGui:FindFirstChild("CatHubOpenClose"):Destroy()
+end
+
+local UIStroke = Instance.new("UIStroke")
+local UICorner = Instance.new("UICorner")
+
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "CatHubOpenClose"
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
+ImageButton.Draggable = true
+ImageButton.Position = UDim2.new(0.102097899, 0, 0.0742971897, 0)
+ImageButton.Size = UDim2.new(0, 59, 0, 49)
+ImageButton.Image = "http://www.roblox.com/asset/?id=15348357130"
+
+UICorner.Name = "MainCorner"
+UICorner.CornerRadius = UDim.new(0, 9)
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Down:connect(function()
+        game.CoreGui:FindFirstChild("CatHub").Enabled = not game.CoreGui:FindFirstChild("CatHub").Enabled
+end)
