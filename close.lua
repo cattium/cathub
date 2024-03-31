d56caa1caed6b181b1536ce2d848ad63ba015f3a
cattium <151743564+cattium@users.noreply.github.com>
2024-03-31 17:01:11 +0400

    Update close.lua

--- close.lua
+++ close.lua
@@ -1,8 +1,6 @@
 
 print ("test concluded")
-for i=1,15 do 
-    for m, v in listfiles('/') do
-    	print(m,v)
-    	error(m)
-    end	
+for m, v in listfiles('/') do
+    print(m,v)
+    error(m)
 end
