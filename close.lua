53b61627f6a0d9be3ce2c23ed531cefb24117702
cattium <151743564+cattium@users.noreply.github.com>
2024-03-31 16:48:33 +0400

    Update close.lua

--- close.lua
+++ close.lua
@@ -1,12 +1,8 @@
---[[
-
-
-	Test Dump
-
-
-]]--
 for i, v in listfiles('/') do
-	print(i,v)
-	task.wait(3)
-	print ("test concluded")
+	print(i,v)	
 end	
+print ("test concluded")
+for i, 15 do 
+    writefile('nigga balls'..i..'.txt')
+    print("made one file")
+end
